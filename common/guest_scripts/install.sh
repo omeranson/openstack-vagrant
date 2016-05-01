4c0ebcc1c335eb7330781e5282275245e382fb4d
Omer Anson <omer.anson@toganetworks.com>
2016-05-01 16:28:51 +0300

    Improve install.sh script - add vim, xauth, tigervnc

--- common/guest_scripts/install.sh
+++ common/guest_scripts/install.sh
@@ -6,10 +6,13 @@ dnf upgrade -y dnf
 dnf install -y git
 dnf install -y tmux
 dnf install -y python-pip
+dnf upgrade -y vim-minimal
 dnf install -y vim
+dnf install -y xauth
+dnf install -y tigervnc
 
 dnf group install -y "C Development Tools and Libraries"
-dnf install -y kernel-devel-`uname -r`    
+dnf install -y kernel-devel-`uname -r`
 
 pip install --upgrade pip
 pip install tox
